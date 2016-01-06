a958079fe57f3e6b0e58aa50f90fd4fc781b803a
workslon <workslon@gmail.com>
2016-01-06 13:52:56 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,54 +3,84 @@ alias .. "cd .."
 alias ... "cd ../.."
 alias .... "cd ../../.."
 alias ..... "cd ../../../.."
-alias p "cd ~/Dropbox/public/devel/projects"
-alias mm "cd ~/Dropbox/public/devel/projects/Maxymiser/"
-alias s "cd ~/Dropbox/public/devel/projects/Slon"
-alias htd "/Applications/MAMP/htdocs"
-alias h "history"
+alias se "cd ~/Dropbox\ \(MaxySEDEV\)/Users/Vitaliy"
+alias slon "cd ~/Dropbox\ \(Personal\)/Public/devel/projects/Slon"
+alias mm "cd ~/Dropbox\ \(Personal\)/Public/devel/projects/Maxymiser"
+alias server "python -m SimpleHTTPServer 8000"
+alias proxy "mitmdump -s replace.py"
 
 # git shortcuts
 alias g "git"
 
+function app-redux
+  mkdir build
+  mkdir src
+  touch index.html
+  touch webpack.config.js
+  cd src/
+  mkdir components
+  mkdir reducers
+  mkdir stores
+  touch app.js
+  cd ../
+  sublime (pwd)
+end
+
+function app-flux
+  mkdir build
+  mkdir src
+  touch index.html
+  touch webpack.config.js
+  cd src/
+  mkdir components
+  mkdir constants
+  mkdir dispatchers
+  mkdir actions
+  mkdir stores
+  touch app.js
+  cd ../
+  sublime (pwd)
+end
+
 function fish_prompt
-	# set_color yellow
-	# echo -n (date +'%H:%M') ' '
-	# set_color cyan
-	# echo -n (pwd)
-	# set_color green
-	#echo -n ' ❯'
+  # set_color yellow
+  # echo -n (date +'%H:%M') ' '
+  # set_color cyan
+  # echo -n (pwd)
+  # set_color green
+  #echo -n ' ❯'
 
-	if not set -q -g __fish_robbyrussell_functions_defined
-	set -g __fish_robbyrussell_functions_defined
-	function _git_branch_name
-		echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-	end
+  if not set -q -g __fish_robbyrussell_functions_defined
+  set -g __fish_robbyrussell_functions_defined
+  function _git_branch_name
+    echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  end
 
-	function _is_git_dirty
-		echo (git status -s --ignore-submodules=dirty ^/dev/null)
-	end
-	end
+  function _is_git_dirty
+    echo (git status -s --ignore-submodules=dirty ^/dev/null)
+  end
+  end
 
-	set -l cyan (set_color -o cyan)
-	set -l yellow (set_color -o yellow)
-	set -l red (set_color -o red)
-	set -l blue (set_color -o blue)
-	set -l green (set_color -o green)
-	set -l normal (set_color normal)
+  set -l cyan (set_color -o cyan)
+  set -l yellow (set_color -o yellow)
+  set -l red (set_color -o red)
+  set -l blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -l normal (set_color normal)
 
-	set -l arrow "$yellow" (date +'%H:%M') ' '
-	set -l cwd $cyan(basename (prompt_pwd))
+  set -l arrow "$yellow" (date +'%H:%M') ' '
+  set -l cwd $cyan(basename (prompt_pwd))
 
-	if [ (_git_branch_name) ]
-	set -l git_branch $red(_git_branch_name)
-	set git_info "$blue git:$git_branch$blue"
+  if [ (_git_branch_name) ]
+  set -l git_branch $red(_git_branch_name)
+  set git_info "$blue git:$git_branch$blue"
 
-	if [ (_is_git_dirty) ]
-		set -l dirty "$yellow ✗"
-		set git_info "$git_info$dirty"
-	end
-	end
+  if [ (_is_git_dirty) ]
+    set -l dirty "$yellow ✗"
+    set git_info "$git_info$dirty"
+  end
+  end
 
-	echo -n -s $arrow $cwd $git_info $green ' › ' $normal
+  echo -n -s $arrow $cwd $git_info $green ' › ' $normal
 
 end
