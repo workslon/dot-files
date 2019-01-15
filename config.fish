9dd62999901eabf11c768a243d5cb67c3f00da97
workslon <workslon@gmail.com>
2019-01-15 16:38:28 +0100

    ~

--- config.fish
+++ config.fish
@@ -64,16 +64,14 @@ end
 # if it does not exist, create the file
 setenv SSH_ENV $HOME/.ssh/environment
 
-function start_agent
-    echo "Initializing new SSH agent ..."
-    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-    chmod 600 $SSH_ENV
-    . $SSH_ENV > /dev/null
-    ssh-add
-end
+# start SSH agent
+echo "Initializing new SSH agent ..."
+ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+chmod 600 $SSH_ENV
+. $SSH_ENV > /dev/null
+ssh-add ~/.ssh/github_vitaliysobur
 
-eval (python2.7 -m virtualfish)
-
+# add golang
 set -x GOPATH $HOME/go
 set -x PATH $PATH $HOME/go/bin $GOPATH/bin
 set -x PATH $PATH $HOME/go/bin $GOROOT/bin
